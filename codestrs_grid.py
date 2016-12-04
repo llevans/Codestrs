fdf12b6b19e7544c6edbc0ada5f1467e7a129e02
Lyn Evans <lyn.evans@ge.com>
2016-12-04 15:11:27 -0600

    basic grid

--- codestrs_grid.py
+++ codestrs_grid.py
@@ -7,18 +7,22 @@ sprite.set_gravity_off()
 
 click_counter = 0
 
-def handle_space_key():
+def handle_pedal():
     global click_counter
     click_counter = click_counter + 1
+    
+    # If click on space bar is odd - start pedalling 
     if click_counter % 2 != 0:
         sprite.set_x_speed(5)
+
+    # If click on space bar is even - stop pedalling 
     else:
         x = sprite.get_x()
         y = sprite.get_y()
         sprite.go_to(x, y)
     # add other actions...
 
-stage.event_space_key(handle_space_key)
+stage.event_space_key(handle_pedal)
 
 
 
