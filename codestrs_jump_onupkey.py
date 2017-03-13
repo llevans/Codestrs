feb3839ab670a157062a5d9ceaa2d189815a5b15
Lyn L Evans <lyn_evans@bellsouth.net>
2017-03-13 10:30:18 -0500

    Update codestrs_jump_onupkey.py

--- codestrs_jump_onupkey.py
+++ codestrs_jump_onupkey.py
@@ -5,7 +5,7 @@
 #  Description : Render a bike sprite using the Codestrs online Python interpreter    # 
 #                Move the bike forward at the Righ Arrow click event                  # 
 #                Solution to have the bike jump upwards at the Up Arrow click event   #
-*                                                                                     #   
+#                                                                                     #   
 #######################################################################################
 import time
 
